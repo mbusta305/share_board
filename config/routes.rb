519e2f785cf49a2fa300d1f88b638c0327f969f4
Michael Busta <busta2356@gmail.com>
2016-06-03 18:15:43 -0400

    Finish styling / started adding Upvote

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
-Rails.application.routes.draw do
+ Rails.application.routes.draw do
   devise_for :users
-  resources :pins
+  resources :pins do
+    put "like", to: "pins#upvote"
+  end
 
   root "pins#index"
 end
